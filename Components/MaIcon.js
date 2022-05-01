6f73cb4e0d4e3d64f342fbf377d7261a2969360b
AndomiAnsari <andomiuddeen@gmail.com>
2022-04-30 23:02:02 -0700

    last stage

--- Components/MaIcon.js
+++ Components/MaIcon.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const MaIcon = props => {
